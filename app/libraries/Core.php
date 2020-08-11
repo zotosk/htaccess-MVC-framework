738cf77944e0ce889e8e5bc6efa0d0dd32db3439
unknown <kzwtos@gmail.com>
2020-08-11 16:49:15 +0300

    explode func

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -22,7 +22,8 @@ class Core
         if (isset($_GET['url'])) {
             $url = rtrim($_GET['url'], '/');
             $url = filter_var($url, FILTER_SANITIZE_URL);
-            var_dump($url);
+            $url = explode('/', $url);
+            return $url;
         }
     }
 }
